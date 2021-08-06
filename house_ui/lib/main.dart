e8cdc8de1584a4021a456eca5d6a393e1bf58e9f
Patrick Witter <pwitterop123@gmail.com>
2021-08-06 15:51:36 -0500

    Added ' house_ui ' project to folder

---
+++ house_ui/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:house_ui/screens/LandingPage.dart';
+import 'package:house_ui/utils/constants.dart';
+import 'dart:ui';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = window.physicalSize.width;
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Design 1',
+      theme: ThemeData(
+          primaryColor: COLOR_WHITE,
+          accentColor: COLOR_DARK_BLUE,
+          textTheme: screenWidth < 500 ? TEXT_THEME_SMALL : TEXT_THEME_DEFAULT,
+          fontFamily: "Montserrat"),
+      home: LandingPage(),
+    );
+  }
+}
---
+++ house_ui/lib/widgets/OptionButton.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:house_ui/utils/constants.dart';
+//import 'package:house_ui/utils/widget_functions.dart';
+
+class OptionButton extends StatelessWidget {
+  final String text;
+  final IconData icon;
+  final double width;
+
+  const OptionButton(
+      {Key key, @required this.text, @required this.icon, @required this.width})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      child: FlatButton(
+          color: COLOR_DARK_BLUE,
+          splashColor: Colors.white.withAlpha(55),
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+          onPressed: () {},
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(
+                icon,
+                color: COLOR_WHITE,
+              ),
+              //addHorizontalSpace(1),
+              Text(
+                text,
+                style: TextStyle(color: COLOR_WHITE),
+              )
+            ],
+          )),
+    );
+  }
+}
