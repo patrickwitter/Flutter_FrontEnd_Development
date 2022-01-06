7f8e2854443d5dccd93e9a33f84287d80d1b5b76
Patrick Witter <pwitterop123@gmail.com>
2022-01-06 13:09:49 -0500

    Completed travel UI

--- traveldiary/lib/main.dart
+++ traveldiary/lib/main.dart
@@ -29,81 +29,9 @@ class Home extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(18.0),
         child: ListView(shrinkWrap: true, children: [
-          Row(
-            children: [
-              Text(
-                "travelogram",
-                style: Theme.of(context)
-                    .textTheme
-                    .headline1!
-                    .copyWith(fontSize: 20, fontWeight: FontWeight.w800),
-              ),
-              const Expanded(child: SizedBox()),
-              IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  Icons.notifications,
-                  color: Colors.grey,
-                  size: 30,
-                ),
-              ),
-              Container(
-                width: 40,
-                height: 40,
-                decoration: const BoxDecoration(
-                  shape: BoxShape.circle,
-                  image: DecorationImage(
-                    image: AssetImage("lib/assets/images/chris.jpg"),
-                  ),
-                ),
-              )
-            ],
-          ),
+          const Header(),
           const SizedBox(height: 20),
-          Container(
-            decoration: BoxDecoration(
-              color: Colors.grey[100],
-              borderRadius: BorderRadius.circular(10),
-            ),
-            padding: const EdgeInsets.all(20),
-            child: Row(
-              children: [
-                Transform.rotate(
-                  angle: 45,
-                  child: Container(
-                    decoration: BoxDecoration(
-                        color: Colors.blue[300],
-                        borderRadius: BorderRadius.circular(10)),
-                    padding: const EdgeInsets.all(5),
-                    child: const Icon(
-                      Icons.navigation,
-                      color: Colors.white,
-                      size: 20,
-                    ),
-                  ),
-                ),
-                const SizedBox(width: 20),
-                Column(
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text("maldives trip 2018".toUpperCase(),
-                        style: Theme.of(context).textTheme.bodyText1!.copyWith(
-                              color: Colors.grey,
-                            )),
-                    Text("Add an Update",
-                        style: Theme.of(context).textTheme.bodyText1)
-                  ],
-                ),
-                const Expanded(child: SizedBox()),
-                const Icon(
-                  Icons.navigate_next,
-                  color: Colors.grey,
-                  size: 30,
-                )
-              ],
-            ),
-          ),
+          const SecondHeader(),
           const SizedBox(
             height: 10,
           ),
@@ -130,9 +58,137 @@ class Home extends StatelessWidget {
             image2: "lib/assets/images/beach2.jpg",
             image3: "lib/assets/images/beach3.jpg",
             additional: 50,
+          ),
+          PicGrid(
+            header: Row(
+              children: [
+                Text(
+                  "Maui Summer 2018 ",
+                  style: Theme.of(context).textTheme.headline1!.copyWith(
+                        fontWeight: FontWeight.bold,
+                      ),
+                ),
+                const Expanded(
+                  child: SizedBox(),
+                ),
+                Transform.rotate(
+                  angle: 45,
+                  child: Icon(
+                    Icons.navigation_outlined,
+                    size: 35,
+                    color: Colors.grey[600],
+                  ),
+                ),
+                Icon(
+                  Icons.favorite_border_outlined,
+                  size: 35,
+                  color: Colors.grey[600],
+                ),
+              ],
+            ),
+            image1: "lib/assets/images/beach3.jpg",
+            image2: "lib/assets/images/beach1.jpg",
+            image3: "lib/assets/images/beach2.jpg",
+            additional: 50,
           )
         ]),
       ),
+    );
+  }
+}
+
+class SecondHeader extends StatelessWidget {
+  const SecondHeader({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.grey[100],
+        borderRadius: BorderRadius.circular(10),
+      ),
+      padding: const EdgeInsets.all(20),
+      child: Row(
+        children: [
+          Transform.rotate(
+            angle: 45,
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.blue[300],
+                  borderRadius: BorderRadius.circular(10)),
+              padding: const EdgeInsets.all(5),
+              child: const Icon(
+                Icons.navigation,
+                color: Colors.white,
+                size: 25,
+              ),
+            ),
+          ),
+          const SizedBox(width: 20),
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("maldives trip 2018".toUpperCase(),
+                  style: Theme.of(context).textTheme.bodyText1!.copyWith(
+                        color: Colors.grey,
+                      )),
+              Text("Add an Update",
+                  style: Theme.of(context).textTheme.bodyText1!.copyWith(
+                        fontSize: 18,
+                      ))
+            ],
+          ),
+          const Expanded(child: SizedBox()),
+          const Icon(
+            Icons.navigate_next,
+            color: Colors.grey,
+            size: 30,
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class Header extends StatelessWidget {
+  const Header({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text(
+          "travelogram",
+          style: Theme.of(context)
+              .textTheme
+              .headline1!
+              .copyWith(fontSize: 20, fontWeight: FontWeight.w800),
+        ),
+        const Expanded(child: SizedBox()),
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            Icons.notifications,
+            color: Colors.grey,
+            size: 30,
+          ),
+        ),
+        Container(
+          width: 40,
+          height: 40,
+          decoration: const BoxDecoration(
+            shape: BoxShape.circle,
+            image: DecorationImage(
+              image: AssetImage("lib/assets/images/chris.jpg"),
+            ),
+          ),
+        )
+      ],
     );
   }
 }
@@ -155,15 +211,11 @@ class PicGrid extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final maxWidgetHeight = MediaQuery.of(context).size.height * 1 / 2;
-    return LimitedBox(
-      // The incomming height is uncontrained as we are in a list view. Hence we use limited box,
-      // This is beacuse we want to tell the row to take up the remaining width and height.
-      // If this was ommited the flexible-row would get infinite vertical space and throw an error
-      maxHeight: maxWidgetHeight,
-      child: LayoutBuilder(builder: (context, constraints) {
-        print("${constraints.maxHeight}");
-        print("${constraints.minHeight}");
-        return Column(
+    return Padding(
+      padding: const EdgeInsets.only(top: 18.0),
+      child: LimitedBox(
+        maxHeight: maxWidgetHeight,
+        child: Column(
           mainAxisSize: MainAxisSize.min,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
@@ -172,11 +224,6 @@ class PicGrid extends StatelessWidget {
               height: 10,
             ),
             Flexible(
-              // We use this as we want to tell the next column to take up the rest of the vertical space.
-              // If we did not do this the row would have unbounded vertical space (as it is a non-flex child of the column parent).
-              // And then pass this height to its children
-              // Due to this the column in the "Expanded [denoted 'here']" widget would have infinite height if this was ommited.
-              // As the expanded says "Take up the remainig space" the column child would have infinite height
               child: Row(children: [
                 Container(
                   width: MediaQuery.of(context).size.width * 1 / 2.5,
@@ -196,68 +243,60 @@ class PicGrid extends StatelessWidget {
                   width: 2,
                 ),
                 Expanded(
-                  // here - The expanded tells the column to take up remaining width and height within the row
-                  child: LayoutBuilder(builder: (context, constraints) {
-                    print("${constraints.maxHeight}");
-                    print("${constraints.minHeight}");
-                    print(" width - ${constraints.minWidth}");
-                    print(" width - ${constraints.maxWidth}");
-                    return Column(
-                      children: [
-                        Expanded(
-                          child: Container(
-                            decoration: BoxDecoration(
-                              image: DecorationImage(
-                                fit: BoxFit.fill,
-                                image: AssetImage(image2),
-                              ),
-                              borderRadius: const BorderRadius.only(
-                                topRight: Radius.circular(10),
-                              ),
+                  child: Column(
+                    children: [
+                      Expanded(
+                        child: Container(
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              fit: BoxFit.fill,
+                              image: AssetImage(image2),
+                            ),
+                            borderRadius: const BorderRadius.only(
+                              topRight: Radius.circular(10),
                             ),
                           ),
                         ),
-                        const SizedBox(
-                          height: 2,
+                      ),
+                      const SizedBox(
+                        height: 2,
+                      ),
+                      Expanded(
+                        child: GestureDetector(
+                          onTap: () {},
+                          child: Stack(children: [
+                            Container(
+                              decoration: BoxDecoration(
+                                image: DecorationImage(
+                                  fit: BoxFit.fill,
+                                  image: AssetImage(image3),
+                                ),
+                                borderRadius: const BorderRadius.only(
+                                  bottomRight: Radius.circular(10),
+                                ),
+                              ),
+                            ),
+                            Align(
+                              alignment: Alignment.bottomRight,
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Text(
+                                  '+' + additional.toString(),
+                                  style: Theme.of(context).textTheme.subtitle1,
+                                ),
+                              ),
+                            ),
+                          ]),
                         ),
-                        Expanded(
-                          child: GestureDetector(
-                            onTap: () {},
-                            child: Stack(children: [
-                              Container(
-                                decoration: BoxDecoration(
-                                  image: DecorationImage(
-                                    fit: BoxFit.fill,
-                                    image: AssetImage(image3),
-                                  ),
-                                  borderRadius: const BorderRadius.only(
-                                    bottomRight: Radius.circular(10),
-                                  ),
-                                ),
-                              ),
-                              Align(
-                                alignment: Alignment.bottomRight,
-                                child: Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Text(
-                                    '+' + additional.toString(),
-                                    style:
-                                        Theme.of(context).textTheme.subtitle1,
-                                  ),
-                                ),
-                              ),
-                            ]),
-                          ),
-                        )
-                      ],
-                    );
-                  }),
-                )
+                      )
+                    ],
+                  ),
+                ),
               ]),
             ),
           ],
-        );
-      }),
+        ),
+      ),
     );
   }
 }
--- traveldiary/lib/theme.dart
+++ traveldiary/lib/theme.dart
@@ -1,20 +1,25 @@
 import 'package:flutter/material.dart';
 
 ThemeData appTheme = ThemeData.light().copyWith(
+    textTheme: appTextTheme,
     primaryColor: Colors.blue,
     colorScheme: const ColorScheme.light().copyWith(
       secondary: Colors.grey,
     ));
 
 TextTheme appTextTheme = const TextTheme(
+  headline1: TextStyle(
+    fontSize: 24,
+    color: Color(0xFF303030),
+  ),
   bodyText1: TextStyle(
     color: Colors.black,
     fontWeight: FontWeight.bold,
-    fontSize: 10,
+    fontSize: 14,
   ),
   subtitle1: TextStyle(
     color: Colors.white,
-    fontSize: 10,
+    fontSize: 16,
     fontWeight: FontWeight.bold,
   ),
 );
